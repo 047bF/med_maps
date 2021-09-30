db21dcd8be86f4e0818c263843ce22d8fc10c707
047bF <50794098+047bF@users.noreply.github.com>
2021-10-01 00:28:26 +0300

    add requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='med_maps',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='woodim',
+    author_email='',
+    description=''
+)
